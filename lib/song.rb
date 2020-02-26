082aebcbb132da8874b2f43db2b850befcb353ea
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-26 09:37:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,7 @@ class Song
   attr_accessor :name
   attr_reader :artist
   
-  extend Memorable::ClassMethods
-  extend Findable::ClassMethods
+  extend Memorable::ClassMethods, Findable::ClassMethods
   
   
   @@songs = []
